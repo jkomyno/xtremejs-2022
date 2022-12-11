c8f74bd71e65d78162275c01a8d418a628d91a60
jkomyno <skiabo97@gmail.com>
2022-12-11 22:06:56 +0100

    chore: removed unused template packages

--- packages/common-utils/src/sum.ts
+++
@@ -1,1 +0,0 @@
-export const sum = (arr: number[]) => arr.reduce((acc, curr) => acc + curr, 0)
--- packages/example/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { sum } from '@jkomyno/common-utils'
-
-function main() {
-  const arr = [1, 2, 3]
-  console.log(`Summing ${arr.join(', ')} results in ${sum(arr)}`)
-}
-
-main()
